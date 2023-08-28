9cb5622ed95004ab101f935dc89aad9c62be2ab8
Onur Tuncay Bal <94485797+welkiej7@users.noreply.github.com>
2023-08-28 23:14:17 +0300

    Update get_sol_frame.R
    removed get links option function, now returns a data frame instead of a vector.

--- get_sol_frame.R
+++ get_sol_frame.R
@@ -1,24 +1,10 @@
 get_sol_frame <- function(){
-rvest::read_html("https://eksisozluk1923.com") -> content.html
-content.html%>%
-html_elements(xpath = "/html[1]/body[1]/div[2]/div[1]/nav[1]/ul[1]")%>%
-rvest::html_text()  -> frame
-
-stringr::str_remove_all(frame, '\n') -> frame
-frame%>%stringr::str_split("\r") -> frame
-frame[[1]] -> frame
-sol_frame_fin <- c()
-for(i in 1:length(frame)){
-    if(stringr::str_count(frame[i]) >= 13 ){
-    sol_frame_fin <- append(sol_frame_fin, frame[i], after = 1)
-    } 
-}
-return(sol_frame_fin)
-}
-
-get_sol_frame_links <- function(){
-    url <- "https://eksisozluk1923.com"
-    read_html(url)%>%html_nodes('#partial-index > ul')%>%
+  url <- "https://eksisozluk1923.com"
+  read_html(url)%>%html_nodes('#partial-index > ul')%>%
     html_elements("a")%>%html_attrs() -> frame.attr
-    return(unlist(frame.attr))
+  frame.attr <- unlist(frame.attr)
+  frame.attr <- str_remove_all(frame.attr, pattern = fixed("?a=popular"))
+  frame.attr <- as.data.frame(str_split_fixed(frame.attr, pattern = "--", n =2 ))
+  colnames(frame.attr) <- c("Topic","Identifier")
+  return(frame.attr)
 }
