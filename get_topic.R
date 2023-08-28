a8d1361ffe628a566f2b7bbc6b0097c8d098b714
Onur Tuncay Bal <94485797+welkiej7@users.noreply.github.com>
2023-08-28 23:13:32 +0300

    Update get_topic.R
    updated refresh interval to avoid 502

--- get_topic.R
+++ get_topic.R
@@ -1,29 +1,16 @@
-get_topic <- function(main = "https://eksisozluk1923.com/", link, leftover = FALSE){
+get_topic <- function(main = "https://eksisozluk1923.com", link, leftover = FALSE, request.rate = 0.5){
   main.page <- rvest::read_html(paste(main,link, "?p=1", sep = ""))
   page.count <-main.page%>%
-      html_elements(xpath = "/html/body/div[2]/div[2]/div[2]/section/div[1]/div[1]/div[2]")%>%
-      html_attr("data-pagecount")
+    html_elements(xpath = "/html/body/div[2]/div[2]/div[2]/section/div[1]/div[1]/div[2]")%>%
+    html_attr("data-pagecount")
   page.count <- as.numeric(page.count)
   results <- as.data.frame(matrix(NA_real_, nrow = 0, ncol = 3))
   colnames(results) <- c("Suser","Content","Date")
-  if(!leftover){
+  message(paste("Retrieving",link))
+  if(leftover == FALSE){
     pbar <- txtProgressBar(min = 0, max = page.count, char = "≈", style = 3)
     message("New Iteration in Progress...")
-      for(page in 1:page.count){
-         moving.page <- rvest::read_html(paste(main,link,"?p=", page, sep = ""))
-         users_vector <- moving.page%>%html_nodes(".entry-author")%>%html_text2()
-         content_vector <- moving.page%>%html_nodes(".content")%>%html_text2()%>%stringr::str_remove_all("\n")
-         date_vector <- moving.page%>%html_nodes(".entry-date")%>%html_text2()   
-         temp.frame <- data.frame(users_vector, content_vector, date_vector)
-         colnames(temp.frame) <- colnames(results)
-         results <- rbind(results,temp.frame)
-         rm(temp.frame)
-         setTxtProgressBar(value = page, pb = pbar)       
-      }
-   } else {
-    pbar <- txtProgressBar(min = leftover, max = page.count, char = "≈", style = 3)
-    message("Updating the Topic")
-    for(page in leftover:page.count){
+    for(page in 1:page.count){
       moving.page <- rvest::read_html(paste(main,link,"?p=", page, sep = ""))
       users_vector <- moving.page%>%html_nodes(".entry-author")%>%html_text2()
       content_vector <- moving.page%>%html_nodes(".content")%>%html_text2()%>%stringr::str_remove_all("\n")
@@ -32,8 +19,27 @@ get_topic <- function(main = "https://eksisozluk1923.com/", link, leftover = FAL
       colnames(temp.frame) <- colnames(results)
       results <- rbind(results,temp.frame)
       rm(temp.frame)
-      setTxtProgressBar(value = page, pb = pbar)       
+      setTxtProgressBar(value = page, pb = pbar)  
+      Sys.sleep(request.rate)
+    }
+  } else {
+    pbar <- txtProgressBar(min = leftover, max = page.count + 1, char = "≈", style = 3)
+    message("Updating the Topic")
+    if(leftover != page.count){
+      for(page in leftover:page.count){
+        moving.page <- rvest::read_html(paste(main,link,"?p=", page, sep = ""))
+        users_vector <- moving.page%>%html_nodes(".entry-author")%>%html_text2()
+        content_vector <- moving.page%>%html_nodes(".content")%>%html_text2()%>%stringr::str_remove_all("\n")
+        date_vector <- moving.page%>%html_nodes(".entry-date")%>%html_text2()   
+        temp.frame <- data.frame(users_vector, content_vector, date_vector)
+        colnames(temp.frame) <- colnames(results)
+        results <- rbind(results,temp.frame)
+        rm(temp.frame)
+        setTxtProgressBar(value = page, pb = pbar)
+        Sys.sleep(request.rate)
+      }
     }
   }
-  return(results)
+  
+  return(list(results, page.count))
 }
